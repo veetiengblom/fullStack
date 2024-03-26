d57125bc31e8ce1c760e0dd274b837715ec100a5
veetiengblom <Veeti_engblom@hotmail.com>
2024-03-26 12:40:00 +0000

    Deleted test, finished osa2 tasks

--- osa2/maidenTiedot/src/services/weatherServices.jsx
+++ osa2/maidenTiedot/src/services/weatherServices.jsx
@@ -2,6 +2,7 @@ import axios from "axios";
 const apiKey = import.meta.env.VITE_API_KEY;
 
 const weather = (capital) => {
+  console.log("weather");
   const request = axios.get(
     `https://api.openweathermap.org/data/2.5/weather?q=${capital}&appid=${apiKey}&units=metric`
   );
